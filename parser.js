d6efb7d0a8c694ab6c5fcbb97e1696df9e734488
Valeria Zherditskaia <st.leron@mail.ru>
2025-08-27 16:07:59 +0300

    Реализовала парсинг файлов с поддержкой относительных и абсолютных путей

--- parser.js
+++ parser.js
@@ -1,6 +1,9 @@
 import fs from 'fs'
+import path from 'path'
 
 const parseFile = (filepath) => {
+  const cwd = process.cwd()
+
   const fileContent = fs.readFileSync(filepath, 'utf-8')
   return JSON.parse(fileContent)
 }
